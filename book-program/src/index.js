1516d89eb63623634dea1c90ff36010409b14393
amansagar0403 <as624@snu.edu.in>
2024-01-13 16:15:42 +0530

    images and css done

--- book-program/src/index.js
+++ book-program/src/index.js
@@ -1,23 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const Booklist = () =>{
-    return <section>
+import "./index.css"
+
+const Booklist = () => {
+    return <section className='booklist'>
+        <Book></Book>
+        <Book></Book>
+        <Book></Book>
         <Book></Book>
     </section>
 }
 
 const Book = () => {
-    return <article>
-    <Image></Image>
-    <Title></Title>
-    <Author></Author>
-</article>
+    return <article className='book'>
+        <Image></Image>
+        <Title></Title>
+        <Author></Author>
+    </article>
 }
 
-const Image = () => <h2>Image placeholder</h2>
-const Title = () => <h2>Book Title</h2>
-const Author = () => <h2>Author</h2>
+const Image = () => <img src='https://m.media-amazon.com/images/I/715qi-cIbML._SL1500_.jpg' alt='The Everything Store: Jeff Bezos and the Age of Amazon' />
+const Title = () => <h2>Don't Believe Everything You Think</h2>
+const Author = () => <h2>Joseph Nguyen</h2>
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<Booklist />)
