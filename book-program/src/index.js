926b5a3c68fa2b920a733661b6dd43c8790e6499
amansagar0403 <as624@snu.edu.in>
2024-01-14 20:33:35 +0530

    changing one component

--- book-program/src/index.js
+++ book-program/src/index.js
@@ -16,16 +16,17 @@ const secondBook = {
 
 const Booklist = () => {
     return <section className='booklist'>
-        <Book author={firstBook.author} title={firstBook.title} image={firstBook.image}></Book>
+        <Book author={firstBook.author} title={firstBook.title} image={firstBook.image}><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla dui libero, viverra ut est nec, aliquam congue ipsum. Vestibulum semper erat vel risus aliquet, a vestibulum nisl rhoncus. Donec commodo tortor et quam pellentesque lobortis. Sed a feugiat dolor. Aliquam erat volutpat. Phasellus venenatis eu ligula ac dignissim.</p><button>Click me</button></Book>
         <Book author={secondBook.author} title={secondBook.title} image={secondBook.image}></Book>
     </section>
 }
 
-const Book = (props) => {
+const Book = ({ image, author, title, children }) => {
     return <article className='book'>
-        <img src={props.image} alt={props.title} />
-        <h2>{props.title}</h2>
-        <h4>{props.author}</h4>
+        <img src={image} alt={title} />
+        <h2>{title}</h2>
+        <h4>{author}</h4>
+        {children}
     </article>
 }
 const root = ReactDOM.createRoot(document.getElementById('root'));
